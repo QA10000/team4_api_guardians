f72151f04c86332c7a816e2b3c0d2e3a238af43b
sindhuaccet07 <sindhuaccet07@gmail.com>
2026-01-15 23:58:12 -0500

    merge changes

--- src/test/java/com/lms/pojo/LoginRequest.java
+++ src/test/java/com/lms/pojo/LoginRequest.java
@@ -31,33 +31,4 @@ public class LoginRequest {
         this.password = password;
     }
 
-    @JsonProperty("userLoginEmailId")
-    private String userLoginEmailId;
-
-    @JsonProperty("password")
-    private String password;
-
-    public LoginRequest() {}
-
-    public LoginRequest(String userLoginEmailId, String password) {
-        this.userLoginEmailId = userLoginEmailId;
-        this.password = password;
-    }
-
-    public String getUserLoginEmailId() {
-        return userLoginEmailId;
-    }
-
-    public void setUserLoginEmailId(String userLoginEmailId) {
-        this.userLoginEmailId = userLoginEmailId;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
-
